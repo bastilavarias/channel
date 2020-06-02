f1f6d01bd6bcc6032d597c3c77256228e10e552c
Sebastian Curtis T. Lavarias <denzilbao@gmail.com>
2020-06-02 20:50:24 +0800

    search room function api created.

--- src/features/room/model.js
+++ src/features/room/model.js
@@ -34,7 +34,29 @@ const roomModel = {
       .where("name", "like", `%${keyword}%`)
       .offset(offset)
       .limit(20)
-      .orderBy("name", "asc");
+      .orderBy("name", "asc")
+      .then(async (result) => {
+        return Promise.all(
+          result.map(async (room) => {
+            const roomInformation = {};
+            roomInformation.id = room.id;
+            roomInformation.name = room.name;
+            roomInformation.type = room.type;
+            roomInformation.avatarUrl = room.avatar_url;
+            roomInformation.admin = await knex("account")
+              .select(["id", "username", "avatar_url"])
+              .where("id", room.account_id)
+              .then((result2) => {
+                const adminInformation = {};
+                adminInformation.id = result2[0].id;
+                adminInformation.username = result2[0].username;
+                adminInformation.avatarUrl = result2[0].avatar_url;
+                return adminInformation;
+              });
+            return roomInformation;
+          })
+        );
+      });
   },
 };
 
--- web-client/src/store/types/room.js
+++ web-client/src/store/types/room.js
@@ -1,1 +1,2 @@
 export const ROOM_CREATE = "ROOM_CREATE";
+export const ROOM_SEARCH = "ROOM_SEARCH";
