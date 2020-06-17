468efaaa8bf96c0092e20171c1867ee233dd573a
Basti Lavarias <denzilbao@gmail.com>
2020-06-17 11:31:47 +0800

    [PROGRESS] created github profile repositories item design.

--- web-client/src/pages/profile/index.vue
+++ web-client/src/pages/profile/index.vue
@@ -10,7 +10,7 @@
             ></v-img>
           </v-avatar>
         </div>
-        <p class="subtitle-2 grey--text">Joined Rooms</p>
+        <custom-label>Joined Rooms</custom-label>
       </v-col>
       <v-col cols="12" md="9">
         <div class="flex-grow-1 mb-10">
@@ -21,9 +21,13 @@
             </h3>
           </div>
         </div>
-        <v-tabs>
+        <v-tabs v-model="tab">
           <template v-for="(tabItem, index) in tabItems">
-            <v-tab :key="index" style="padding-left: 0 !important;">
+            <v-tab
+              :key="index"
+              style="padding-left: 0 !important;"
+              :to="tabItem.to"
+            >
               <v-icon class="mr-2">{{ tabItem.icon }}</v-icon>
               <span class="text-capitalize font-weight-bold">
                 {{ tabItem.text }}
@@ -31,24 +35,35 @@
             </v-tab>
           </template>
         </v-tabs>
+        <v-tabs-items>
+          <router-view></router-view>
+        </v-tabs-items>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import CustomLabel from "../../components/custom/Label";
 export default {
+  components: { CustomLabel },
   data() {
     return {
+      tab: null,
       tabItems: [
         {
           text: "GitHub",
           icon: "mdi-github",
+          to: {
+            name: "profile-github",
+            params: { username: "bastilavarias" },
+          },
         },
 
         {
           text: "About",
           icon: "mdi-account",
+          to: { name: "profile-about" },
         },
       ],
     };
