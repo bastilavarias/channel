863e0dee5f567375bd910b5749425dc47c87794d
Basti Lavarias <denzilbao@gmail.com>
2020-06-18 19:34:19 +0800

    [PRODUCTION TESTING] set up knex vuejs production env.

--- knexfile.js
+++ knexfile.js
@@ -9,4 +9,16 @@ module.exports = {
       directory: __dirname + "/src/db/seeds",
     },
   },
+
+  production: {
+    client: "pg",
+    connection:
+      "postgres://hyeysoihktebaf:ddee74c167868445428113343397e3e94b0a093fdad65c61f7473ab8a0a62dd4@ec2-52-71-231-180.compute-1.amazonaws.com:5432/d5vajd8fs2acpn",
+    migrations: {
+      directory: __dirname + "/src/db/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/src/db/seeds",
+    },
+  },
 };
