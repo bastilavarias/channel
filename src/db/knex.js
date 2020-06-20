5acd1453bae29a36ab4e7f17fc3b3dd82170ef09
Basti Lavarias <denzilbao@gmail.com>
2020-06-20 19:56:41 +0800

    [STAGING] logging knex environment config to test db connection.

--- src/db/knex.js
+++ src/db/knex.js
@@ -1,3 +1,4 @@
 const environment = process.env.ENVIRONMENT || "development";
+console.log(environment);
 const config = require("../../knexfile")[environment];
 module.exports = require("knex")(config);
