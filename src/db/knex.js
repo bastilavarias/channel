2f240bc15d45a73fe298f0057e4bbcacc475a008
Basti Lavarias <denzilbao@gmail.com>
2020-06-20 20:01:53 +0800

    [STAGING] change environment variable.

--- src/db/knex.js
+++ src/db/knex.js
@@ -1,4 +1,4 @@
-const environment = process.env.ENVIRONMENT || "development";
+const environment = process.env.ENVIRONMENT || "production";
 console.log(environment);
 const config = require("../../knexfile")[environment];
 module.exports = require("knex")(config);
