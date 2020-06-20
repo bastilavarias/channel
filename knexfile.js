77dbe012e15e6c6ade9bc3212bdad4759fa5792a
Basti Lavarias <denzilbao@gmail.com>
2020-06-20 19:50:21 +0800

    [STAGING] knex production credentials changed to object model.

--- knexfile.js
+++ knexfile.js
@@ -12,7 +12,13 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+    },
     migrations: {
       directory: __dirname + "/src/db/migrations",
     },
